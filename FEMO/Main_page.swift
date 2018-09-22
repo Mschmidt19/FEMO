1afc30e0c132ec9a11df46e959070d7a29f96dc5
Eunbit Kim <eunbitkim90@gmail.com>
2018-09-22 16:14:55 +0100

    main page connects to space scene

--- FEMO/Main_page.swift
+++ FEMO/Main_page.swift
@@ -10,11 +10,28 @@ import SpriteKit
 
 class Main_page: SKScene {
 
-    var Space_scene: SKSpriteNode!
-    var Desert_scene: SKSpriteNode!
+    var Space_sceneNode: SKSpriteNode!
+    var Desert_sceneNode: SKSpriteNode!
     
     override func didMove(to view: SKView) {
-       Space_scene = self.childNode(withName: "Space_scene") as! SKSpriteNode
-       Desert_scene = self.childNode(withName: "Desert_scene") as! SKSpriteNode
+        Space_sceneNode = self.childNode(withName: "Space_scene") as! SKSpriteNode
+        Desert_sceneNode = self.childNode(withName: "Desert_scene") as! SKSpriteNode
+        
+        Space_sceneNode.texture = SKTexture(imageNamed: "Space_scene")
+        Desert_sceneNode.texture = SKTexture(imageNamed: "Desert_scene")
+        
     }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch = touches.first
+        if let location = touch?.location(in: self) {
+           let node = self.nodes(at: location).first
+            if node?.name  == "Space_scene" || node?.name == "SpaceText" {
+                let transition = SKTransition.doorsOpenHorizontal(withDuration: 0.5)
+                let gameScene = GameScene(fileNamed: "GameScene")
+                self.view?.presentScene(gameScene!, transition: transition)
+            }
+        }
+    }
+    
 }
