0471f36e83137b054ae6b95f461e7834ea2b5e80
Farah Jabri <farahjabri@hotmail.com>
2018-09-24 11:25:56 +0100

    info page goes back to main_page

---
+++ FEMO/Information_Page/InformationScene.swift
@@ -0,0 +1,38 @@
+//
+//  InformationScene.swift
+//  FEMO
+//
+//  Created by Eunbit Evie Kim on 22/09/2018.
+//  Copyright © 2018 FEMO@Makers. All rights reserved.
+//
+
+import SpriteKit
+
+class InformationScene: SKScene {
+    
+    var InformationNode: SKSpriteNode!
+
+    
+    override func didMove(to view: SKView) {
+        InformationNode = self.childNode(withName: "Information_button") as! SKSpriteNode
+        InformationNode.texture = SKTexture(imageNamed: "green_alien")
+        
+        let menu_buttonNode = self.childNode(withName: "Menu_button") as! SKSpriteNode
+        menu_buttonNode.texture = SKTexture(imageNamed: "menu_button")
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch = touches.first
+        
+        if let location = touch?.location(in: self) {
+            let node = self.nodes(at: location)
+            
+          if node.first?.name == "Menu_button" {
+                let transition = SKTransition.doorsCloseHorizontal(withDuration: 0.5)
+                let menuPage = Main_page(fileNamed: "Main_page")
+                self.view?.presentScene(menuPage!, transition: transition)
+            } 
+        }
+    }
+        
+}
